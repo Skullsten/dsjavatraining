f1ed9f0b2fab69c553ff1eb90c1f92b2e037e528
Skullsten <harshitgupta.1092@gmail.com>
2024-07-24 10:44:52 +0530

    unexpected code

---
+++ Unexpected/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args)
+    {
+
+        int i = 5;
+
+        do {
+            i++;
+            System.out.println("GfG");
+        } while (i < 10);
+    }
+}
